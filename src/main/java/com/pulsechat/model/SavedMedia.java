752a11044eb7b1f5cfcad1f1abc9675251b38ff0
Yashraj <parmaryashraj068@gmail.com>
2026-09-02 17:35:12 +0530

    Keep saved media indexing in centralized Mongo configuration

--- src/main/java/com/pulsechat/model/SavedMedia.java
+++ src/main/java/com/pulsechat/model/SavedMedia.java
@@ -2,7 +2,6 @@ package com.pulsechat.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
@@ -12,7 +11,6 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document("saved_media")
-@CompoundIndex(name = "sender_url_unique", def = "{'senderId': 1, 'url': 1}", unique = true)
 public class SavedMedia {
     @Id
     private String id;
