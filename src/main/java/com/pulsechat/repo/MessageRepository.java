bdb2e958f3e61aa40b360ea9c202932018ebb185
Yashraj <parmaryashraj068@gmail.com>
2026-09-02 17:32:39 +0530

    Support saved GIF history backfill queries

--- src/main/java/com/pulsechat/repo/MessageRepository.java
+++ src/main/java/com/pulsechat/repo/MessageRepository.java
@@ -1,8 +1,14 @@
 package com.pulsechat.repo;
+
 import com.pulsechat.model.Message;
+import com.pulsechat.model.MessageType;
 import org.springframework.data.mongodb.repository.*;
+
+import java.util.Collection;
 import java.util.List;
+
 public interface MessageRepository extends MongoRepository<Message,String> {
     List<Message> findTop50ByOrderByCreatedAtDesc();
+    List<Message> findBySenderIdAndTypeIn(String senderId, Collection<MessageType> types);
     long countByDeletedFalse();
 }
