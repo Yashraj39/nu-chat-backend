0c1b53ff3a7e9a4a66dec1718fdcdb3b45495ab9
Yashraj <parmaryashraj068@gmail.com>
2026-09-03 18:02:39 +0530

    Add repository lookup for proxied chat files

--- src/main/java/com/pulsechat/repo/MessageRepository.java
+++ src/main/java/com/pulsechat/repo/MessageRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.mongodb.repository.*;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends MongoRepository<Message,String> {
     List<Message> findTop50ByOrderByCreatedAtDesc();
     List<Message> findBySenderIdAndTypeIn(String senderId, Collection<MessageType> types);
     List<Message> findByTypeIn(Collection<MessageType> types);
     long countByDeletedFalse();
+
+    @Query("{ 'file.publicId': ?0 }")
+    Optional<Message> findByFilePublicId(String publicId);
 }
